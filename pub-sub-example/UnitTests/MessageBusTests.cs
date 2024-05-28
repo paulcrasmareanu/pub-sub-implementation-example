9388ce68aacb9ca5cf8faf33e8ea710c7ea03182
Paul Crasmareanu <paulcrasmareanu@gmail.com>
2024-05-28 12:57:06 +0300

    Add Unit tests for each testable module of application

---
+++ pub-sub-example/UnitTests/MessageBusTests.cs
@@ -0,0 +1,69 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests
+{
+
+  [TestClass]
+  public class MessageBusTests
+  {
+    private MessageBus.MessageBus _messageBus;
+    private Mock<EventHandler<MessageEventArgs>> _handlerMock;
+
+    [TestInitialize]
+    public void Setup()
+    {
+      _messageBus = new MessageBus.MessageBus();
+      _handlerMock = new Mock<EventHandler<MessageEventArgs>>();
+    }
+
+    [TestMethod]
+    public void Subscribe_ShouldInvokeHandlerOnPublish()
+    {
+      // Arrange
+      var topic = Topic.UPPER;
+      var message = "Test Message";
+      _messageBus.Subscribe(topic, _handlerMock.Object);
+
+      // Act
+      _messageBus.Publish(topic, message);
+
+      // Assert
+      _handlerMock.Verify(handler => handler(It.IsAny<object>(), It.Is<MessageEventArgs>(e => e.Message == message && e.Topic == topic)), Times.Once);
+    }
+
+    [TestMethod]
+    public void Unsubscribe_ShouldNotInvokeHandlerOnPublish()
+    {
+      // Arrange
+      var topic = Topic.UPPER;
+      var message = "Test Message";
+      _messageBus.Subscribe(topic, _handlerMock.Object);
+      _messageBus.Unsubscribe(topic, _handlerMock.Object);
+
+      // Act
+      _messageBus.Publish(topic, message);
+
+      // Assert
+      _handlerMock.Verify(handler => handler(It.IsAny<object>(), It.IsAny<MessageEventArgs>()), Times.Never);
+    }
+
+    [TestMethod]
+    public void Subscribe_ShouldNotAddDuplicateHandlers()
+    {
+      // Arrange
+      var topic = Topic.UPPER;
+
+      // Act
+      _messageBus.Subscribe(topic, _handlerMock.Object);
+      _messageBus.Subscribe(topic, _handlerMock.Object);
+
+      // Publish to check if only one handler is called
+      _messageBus.Publish(topic, "Test Message");
+
+      // Assert
+      _handlerMock.Verify(handler => handler(It.IsAny<object>(), It.IsAny<MessageEventArgs>()), Times.Once);
+    }
+  }
+}
---
+++ pub-sub-example/UnitTests/SubscriberTests.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests
+{
+  [TestClass]
+  public class SubscriberTests
+  {
+    private Mock<IMessageBus> _messageBusMock;
+    private Subscriber.Subscriber _subscriber;
+
+    [TestInitialize]
+    public void Setup()
+    {
+      _messageBusMock = new Mock<IMessageBus>();
+      _subscriber = new Subscriber.Subscriber("TestSubscriber", _messageBusMock.Object, Topic.UPPER);
+    }
+
+    [TestMethod]
+    public void Subscribe_ShouldSubscribeToTopic()
+    {
+      // Act
+      _subscriber.Subscribe();
+
+      // Assert
+      _messageBusMock.Verify(bus => bus.Subscribe(Topic.UPPER, It.IsAny<EventHandler<MessageEventArgs>>()), Times.Once);
+    }
+
+    [TestMethod]
+    public void Unsubscribe_ShouldUnsubscribeFromTopic()
+    {
+      // Act
+      _subscriber.Unsubscribe();
+
+      // Assert
+      _messageBusMock.Verify(bus => bus.Unsubscribe(Topic.UPPER, It.IsAny<EventHandler<MessageEventArgs>>()), Times.Once);
+    }
+  }
+}
